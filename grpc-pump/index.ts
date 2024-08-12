82b902d564e48c6e635f31c540d6b8f4bd9e67e9
Mona Paul <ahyammonapaul5@gmail>
2024-08-12 16:07:33 +0100

    GRPC PumpFun

--- grpc-pump/index.ts
+++ grpc-pump/index.ts
@@ -1,131 +1,116 @@
 import Client, {
-    CommitmentLevel,
-    SubscribeRequestAccountsDataSlice,
-    SubscribeRequestFilterAccounts,
-    SubscribeRequestFilterBlocks,
-    SubscribeRequestFilterBlocksMeta,
-    SubscribeRequestFilterEntry,
-    SubscribeRequestFilterSlots,
-    SubscribeRequestFilterTransactions,
-  } from "@triton-one/yellowstone-grpc";
-  import { SubscribeRequestPing } from "@triton-one/yellowstone-grpc/dist/grpc/geyser";
-  import { VersionedTransactionResponse } from "@solana/web3.js";
+  CommitmentLevel,
+  SubscribeRequestAccountsDataSlice,
+  SubscribeRequestFilterAccounts,
+  SubscribeRequestFilterBlocks,
+  SubscribeRequestFilterBlocksMeta,
+  SubscribeRequestFilterEntry,
+  SubscribeRequestFilterSlots,
+  SubscribeRequestFilterTransactions,
+} from "@triton-one/yellowstone-grpc";
+import { SubscribeRequestPing } from "@triton-one/yellowstone-grpc/dist/grpc/geyser";
+import { VersionedTransactionResponse } from "@solana/web3.js";
 import { tOutPut } from "./utils/transactionOutput";
 
 
-  interface SubscribeRequest {
-    accounts: { [key: string]: SubscribeRequestFilterAccounts };
-    slots: { [key: string]: SubscribeRequestFilterSlots };
-    transactions: { [key: string]: SubscribeRequestFilterTransactions };
-    transactionsStatus: { [key: string]: SubscribeRequestFilterTransactions };
-    blocks: { [key: string]: SubscribeRequestFilterBlocks };
-    blocksMeta: { [key: string]: SubscribeRequestFilterBlocksMeta };
-    entry: { [key: string]: SubscribeRequestFilterEntry };
-    commitment?: CommitmentLevel | undefined;
-    accountsDataSlice: SubscribeRequestAccountsDataSlice[];
-    ping?: SubscribeRequestPing | undefined;
-  }
+interface SubscribeRequest {
+  accounts: { [key: string]: SubscribeRequestFilterAccounts };
+  slots: { [key: string]: SubscribeRequestFilterSlots };
+  transactions: { [key: string]: SubscribeRequestFilterTransactions };
+  transactionsStatus: { [key: string]: SubscribeRequestFilterTransactions };
+  blocks: { [key: string]: SubscribeRequestFilterBlocks };
+  blocksMeta: { [key: string]: SubscribeRequestFilterBlocksMeta };
+  entry: { [key: string]: SubscribeRequestFilterEntry };
+  commitment?: CommitmentLevel | undefined;
+  accountsDataSlice: SubscribeRequestAccountsDataSlice[];
+  ping?: SubscribeRequestPing | undefined;
+}
 
-    async function handleStream(client: Client, args: SubscribeRequest) {
-    // Subscribe for events
-    const stream = await client.subscribe();
-  
-    // Create `error` / `end` handler
-    const streamClosed = new Promise<void>((resolve, reject) => {
-      stream.on("error", (error) => {
-        console.log("ERROR", error);
-        reject(error);
-        stream.end();
-      });
-      stream.on("end", () => {
-        resolve();
-      });
-      stream.on("close", () => {
-        resolve();
-      });
+  async function handleStream(client: Client, args: SubscribeRequest) {
+  // Subscribe for events
+  const stream = await client.subscribe();
+
+  // Create `error` / `end` handler
+  const streamClosed = new Promise<void>((resolve, reject) => {
+    stream.on("error", (error) => {
+      console.log("ERROR", error);
+      reject(error);
+      stream.end();
     });
-  
-    // Handle updates
-    stream.on("data", async (data) => {
-      try{
-      const result = await tOutPut(data);
-     console.log(result);
-  }catch(error){
-    if(error){
-      console.log(error)
-    }
+    stream.on("end", () => {
+      resolve();
+    });
+    stream.on("close", () => {
+      resolve();
+    });
+  });
+
+  // Handle updates
+  stream.on("data", async (data) => {
+    try{
+    const result = await tOutPut(data);
+   console.log(result);
+}catch(error){
+  if(error){
+    console.log(error)
   }
+}
 });
-  
-    // Send subscribe request
-    await new Promise<void>((resolve, reject) => {
-      stream.write(args, (err: any) => {
-        if (err === null || err === undefined) {
-          resolve();
-        } else {
-          reject(err);
-        }
-      });
-    }).catch((reason) => {
-      console.error(reason);
-      throw reason;
-    });
-  
-    await streamClosed;
-  }
-  
-  async function subscribeCommand(client: Client, args: SubscribeRequest) {
-    while (true) {
-      try {
-        await handleStream(client, args);
-      } catch (error) {
-        console.error("Stream error, restarting in 1 second...", error);
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  // Send subscribe request
+  await new Promise<void>((resolve, reject) => {
+    stream.write(args, (err: any) => {
+      if (err === null || err === undefined) {
+        resolve();
+      } else {
+        reject(err);
       }
+    });
+  }).catch((reason) => {
+    console.error(reason);
+    throw reason;
+  });
+
+  await streamClosed;
+}
+
+async function subscribeCommand(client: Client, args: SubscribeRequest) {
+  while (true) {
+    try {
+      await handleStream(client, args);
+    } catch (error) {
+      console.error("Stream error, restarting in 1 second...", error);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
   }
-  
-  const client = new Client(
-    'YOUR X URL',
-    'YOUR X TOKEN',
-    undefined,
-  );
-  const req = {
-    accounts: {},
-    slots: {},
-    transactions: {
-      bondingCurve: {
-        vote: false,
-        failed: false,
-        signature: undefined,
-        accountInclude: ['6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P'], //Address 6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P
-        accountExclude: [],
-        accountRequired: [],
-      },
+}
+
+const client = new Client(
+  'YOUR X URL',
+  'YOUR X TOKEN',
+  undefined,
+);
+const req = {
+  accounts: {},
+  slots: {},
+  transactions: {
+    bondingCurve: {
+      vote: false,
+      failed: false,
+      signature: undefined,
+      accountInclude: ['6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P'], //Address 6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P
+      accountExclude: [],
+      accountRequired: [],
     },
-    transactionsStatus: {},
-    entry: {},
-    blocks: {},
-    blocksMeta: {},
-    accountsDataSlice: [],
-    ping: undefined,
-    commitment: CommitmentLevel.CONFIRMED, //for receiving confirmed txn updates
-  };
-  subscribeCommand(client, req);
-  
-//   function decodeRaydiumTxn(tx: VersionedTransactionResponse) {
-//     if (tx.meta?.err) return;
-  
-//     const allIxs = TXN_FORMATTER.flattenTransactionResponse(tx);
-  
-//     const raydiumIxs = allIxs.filter((ix) =>
-//       ix.programId.equals(RAYDIUM_PUBLIC_KEY),
-//     );
-  
-//     const decodedIxs = raydiumIxs.map((ix) =>
-//       RAYDIUM_PARSER.parseInstruction(ix),
-//     );
-  
-//     return decodedIxs;
-//   }
-  
+  },
+  transactionsStatus: {},
+  entry: {},
+  blocks: {},
+  blocksMeta: {},
+  accountsDataSlice: [],
+  ping: undefined,
+  commitment: CommitmentLevel.CONFIRMED, //for receiving confirmed txn updates
+};
+subscribeCommand(client, req);
+
+
