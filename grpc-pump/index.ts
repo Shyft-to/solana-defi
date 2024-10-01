7294d467467d9e54cb9ac1e266a8ccd82f7d07fb
Mona Paul <ahyammonapaul5@gmail>
2024-10-01 16:50:36 +0100

    update

--- grpc-pump/index.ts
+++ grpc-pump/index.ts
@@ -1,129 +1,138 @@
 import Client, {
-    CommitmentLevel,
-    SubscribeRequestAccountsDataSlice,
-    SubscribeRequestFilterAccounts,
-    SubscribeRequestFilterBlocks,
-    SubscribeRequestFilterBlocksMeta,
-    SubscribeRequestFilterEntry,
-    SubscribeRequestFilterSlots,
-    SubscribeRequestFilterTransactions,
-  } from "@triton-one/yellowstone-grpc";
-  import { SubscribeRequestPing } from "@triton-one/yellowstone-grpc/dist/grpc/geyser";
-  import { VersionedTransactionResponse } from "@solana/web3.js";
-import { tOutPut } from "./utils/transactionOutput";
-import { buyTXN } from "./utils/txnParser/buyTxn";
-import { sellTXN } from "./utils/txnParser/sellTxn";
-import { TXN } from "./utils/txnParser/tokenAmount";
-const pumpfun = '6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P';
+  CommitmentLevel,
+  SubscribeRequestAccountsDataSlice,
+  SubscribeRequestFilterAccounts,
+  SubscribeRequestFilterBlocks,
+  SubscribeRequestFilterBlocksMeta,
+  SubscribeRequestFilterEntry,
+  SubscribeRequestFilterSlots,
+  SubscribeRequestFilterTransactions,
+} from "@triton-one/yellowstone-grpc";
+import { SubscribeRequestPing } from "@triton-one/yellowstone-grpc/dist/grpc/geyser";
+import { PublicKey, VersionedTransactionResponse } from "@solana/web3.js";
+import { Idl } from "@project-serum/anchor";
+import { SolanaParser } from "@shyft-to/solana-transaction-parser";
+import { TransactionFormatter } from "./utils/transaction-formatter";
+import pumpFunIdl from "./idls/pump_0.1.0.json";
+import { SolanaEventParser } from "./utils/event-parser";
+import { bnLayoutFormatter } from "./utils/bn-layout-formatter";
+import { transactionOutput } from "./utils/transactionOutput";
 
-  interface SubscribeRequest {
-    accounts: { [key: string]: SubscribeRequestFilterAccounts };
-    slots: { [key: string]: SubscribeRequestFilterSlots };
-    transactions: { [key: string]: SubscribeRequestFilterTransactions };
-    transactionsStatus: { [key: string]: SubscribeRequestFilterTransactions };
-    blocks: { [key: string]: SubscribeRequestFilterBlocks };
-    blocksMeta: { [key: string]: SubscribeRequestFilterBlocksMeta };
-    entry: { [key: string]: SubscribeRequestFilterEntry };
-    commitment?: CommitmentLevel | undefined;
-    accountsDataSlice: SubscribeRequestAccountsDataSlice[];
-    ping?: SubscribeRequestPing | undefined;
-  }
+interface SubscribeRequest {
+  accounts: { [key: string]: SubscribeRequestFilterAccounts };
+  slots: { [key: string]: SubscribeRequestFilterSlots };
+  transactions: { [key: string]: SubscribeRequestFilterTransactions };
+  transactionsStatus: { [key: string]: SubscribeRequestFilterTransactions };
+  blocks: { [key: string]: SubscribeRequestFilterBlocks };
+  blocksMeta: { [key: string]: SubscribeRequestFilterBlocksMeta };
+  entry: { [key: string]: SubscribeRequestFilterEntry };
+  commitment?: CommitmentLevel | undefined;
+  accountsDataSlice: SubscribeRequestAccountsDataSlice[];
+  ping?: SubscribeRequestPing | undefined;
+}
 
-    async function handleStream(client: Client, args: SubscribeRequest) {
-    // Subscribe for events
-    const stream = await client.subscribe();
-  
-    // Create `error` / `end` handler
-    const streamClosed = new Promise<void>((resolve, reject) => {
-      stream.on("error", (error) => {
-        console.log("ERROR", error);
-        reject(error);
-        stream.end();
-      });
-      stream.on("end", () => {
-        resolve();
-      });
-      stream.on("close", () => {
-        resolve();
-      });
+const TXN_FORMATTER = new TransactionFormatter();
+const PUMP_FUN_PROGRAM_ID = new PublicKey(
+  "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P",
+);
+const PUMP_FUN_IX_PARSER = new SolanaParser([]);
+PUMP_FUN_IX_PARSER.addParserFromIdl(
+  PUMP_FUN_PROGRAM_ID.toBase58(),
+  pumpFunIdl as Idl,
+);
+const PUMP_FUN_EVENT_PARSER = new SolanaEventParser([], console);
+PUMP_FUN_EVENT_PARSER.addParserFromIdl(
+  PUMP_FUN_PROGRAM_ID.toBase58(),
+  pumpFunIdl as Idl,
+);
+
+async function handleStream(client: Client, args: SubscribeRequest) {
+  // Subscribe for events
+  const stream = await client.subscribe();
+
+  // Create `error` / `end` handler
+  const streamClosed = new Promise<void>((resolve, reject) => {
+    stream.on("error", (error) => {
+      console.log("ERROR", error);
+      reject(error);
+      stream.end();
     });
-  
-    // Handle updates
-    stream.on("data", async (data) => {
-      try{
-     const result = await tOutPut(data);
-     const buyTxn = buyTXN(result.meta.logMessages);
-     const sellTxn = sellTXN(result.meta.logMessages);
-     const txnParser = await TXN(result.signature.toString());
-     if(buyTxn && txnParser.signer !== undefined){
-      
-      console.log(`
-        TYPE : BUY
-        MINT : ${txnParser.token}
-        SIGNER : ${txnParser.signer}
-        IN  : ${txnParser.solAmount/1000000000} SOL
-        OUT : ${txnParser.tokenAmount}
-        SIGNATURE : ${result.signature}
-        `)
-     }
-      if(sellTxn && txnParser.signer !== undefined){
-      console.log(`
-        TYPE : SELL
-        MINT : ${txnParser.token}
-        SIGNER : ${txnParser.signer}
-        IN : ${txnParser.tokenAmount}
-        SIGNATURE : ${result.signature}
-        `)
-     } 
-  }catch(error){
-    if(error){
-      console.log(error)
+    stream.on("end", () => {
+      resolve();
+    });
+    stream.on("close", () => {
+      resolve();
+    });
+  });
+
+  // Handle updates
+  stream.on("data", (data) => {
+    if (data?.transaction) {
+      const txn = TXN_FORMATTER.formTransactionFromJson(
+        data.transaction,
+        Date.now(),
+      );
+      const parsedTxn = decodePumpFunTxn(txn);
+
+      if (!parsedTxn) return;
+      const tOutput = transactionOutput(parsedTxn)
+
+      console.log(
+        `
+        TYPE : ${tOutput.type}
+        MINT : ${tOutput.mint}
+        SIGNER : ${tOutput.user}
+        TOKEN AMOUNT : ${tOutput.tokenAmount}
+        SOL AMOUNT : ${tOutput.solAmount} SOL
+        SIGNATURE : ${txn.transaction.signatures[0]}
+        `
+      )
     }
-  }
-});
-  
-    // Send subscribe request
-    await new Promise<void>((resolve, reject) => {
-      stream.write(args, (err: any) => {
-        if (err === null || err === undefined) {
-          resolve();
-        } else {
-          reject(err);
-        }
-      });
-    }).catch((reason) => {
-      console.error(reason);
-      throw reason;
-    });
-  
-    await streamClosed;
-  }
-  
-  async function subscribeCommand(client: Client, args: SubscribeRequest) {
-    while (true) {
-      try {
-        await handleStream(client, args);
-      } catch (error) {
-        console.error("Stream error, restarting in 1 second...", error);
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+  });
+
+  // Send subscribe request
+  await new Promise<void>((resolve, reject) => {
+    stream.write(args, (err: any) => {
+      if (err === null || err === undefined) {
+        resolve();
+      } else {
+        reject(err);
       }
+    });
+  }).catch((reason) => {
+    console.error(reason);
+    throw reason;
+  });
+
+  await streamClosed;
+}
+
+async function subscribeCommand(client: Client, args: SubscribeRequest) {
+  while (true) {
+    try {
+      await handleStream(client, args);
+    } catch (error) {
+      console.error("Stream error, restarting in 1 second...", error);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
   }
-  
-  const client = new Client(
-    'gRPC REGION URL',
-    'gRPC TOKEN',
-    undefined,
-  );
-const req = {
+}
+
+const client = new Client(
+  'gRPC REGION URL',
+  'gRPC TOKEN',
+  undefined,
+);
+
+const req: SubscribeRequest = {
   accounts: {},
   slots: {},
   transactions: {
-    bondingCurve: {
+    pumpFun: {
       vote: false,
       failed: false,
       signature: undefined,
-      accountInclude: ['6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P'], //Address 6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P
+      accountInclude: [PUMP_FUN_PROGRAM_ID.toBase58()],
       accountExclude: [],
       accountRequired: [],
     },
@@ -134,7 +143,26 @@ const req = {
   blocksMeta: {},
   accountsDataSlice: [],
   ping: undefined,
-  commitment: CommitmentLevel.CONFIRMED, //for receiving confirmed txn updates
+  commitment: CommitmentLevel.CONFIRMED,
 };
-  subscribeCommand(client, req);
-  
+
+subscribeCommand(client, req);
+
+function decodePumpFunTxn(tx: VersionedTransactionResponse) {
+  if (tx.meta?.err) return;
+
+  const paredIxs = PUMP_FUN_IX_PARSER.parseTransactionData(
+    tx.transaction.message,
+    tx.meta.loadedAddresses,
+  );
+
+  const pumpFunIxs = paredIxs.filter((ix) =>
+    ix.programId.equals(PUMP_FUN_PROGRAM_ID),
+  );
+
+  if (pumpFunIxs.length === 0) return;
+  const events = PUMP_FUN_EVENT_PARSER.parseEvent(tx);
+  const result = { instructions: pumpFunIxs, events };
+  bnLayoutFormatter(result);
+  return result;
+}
--- grpc-pump/utils/txnParser/sellTxn.ts
+++
@@ -1,6 +0,0 @@
-export function sellTXN(logMessages:[]){
-    const logSlice:any[] = logMessages.slice(0, 10);
-    const containSell = logSlice?.some(instruction=> instruction.match(instruction.match(/Sell/i)));
-    if(!containSell)return undefined;
-    return containSell;
-} 
