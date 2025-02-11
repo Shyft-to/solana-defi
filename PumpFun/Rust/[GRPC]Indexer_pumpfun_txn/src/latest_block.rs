d28baa6f90922d3b600d4f31665c5fbf57d6ab71
ahyammona <37624177+ahyammona@users.noreply.github.com>
2025-02-11 22:39:03 +0100

    Update latest_block.rs

--- PumpFun/Rust/[GRPC]Indexer_pumpfun_txn/src/latest_block.rs
+++ PumpFun/Rust/[GRPC]Indexer_pumpfun_txn/src/latest_block.rs
@@ -3,10 +3,12 @@ use solana_client::client_error::ClientError;
 use anyhow::Context;
 
 
-pub fn get_latest_slot() ->  anyhow::Result<u64>{
-    let rpc_url = "https://rpc.va.shyft.to?api_key=your_api"; 
-    let client = RpcClient::new(rpc_url.to_string());
+const API_KEY: &str = "api_key";
 
+pub fn get_latest_slot() -> anyhow::Result<u64> {
+    let rpc_url = format!("https://rpc.va.shyft.to?api_key={}", API_KEY);
+    
+    let client = RpcClient::new(rpc_url);
+    
     client.get_slot().context("Failed to fetch latest slot")
-
 }
