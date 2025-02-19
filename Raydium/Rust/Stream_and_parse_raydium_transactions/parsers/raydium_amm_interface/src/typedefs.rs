8bab0b8afc77dcc0de5fdae4023ef678629cc25e
Abhi <abhirup@shyft.to>
2025-02-20 04:14:47 +0530

    Add stream and parse raydium and token program instruction

---
+++ Raydium/Rust/Stream_and_parse_raydium_transactions/parsers/raydium_amm_interface/src/typedefs.rs
@@ -0,0 +1,75 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::pubkey::Pubkey;
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct WithdrawDestToken {
+    pub withdraw_amount: u64,
+    pub coin_amount: u64,
+    pub pc_amount: u64,
+    pub dest_token_coin: Pubkey,
+    pub dest_token_pc: Pubkey,
+}
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct WithdrawQueue {
+    pub owner: [u64; 4],
+    pub head: u64,
+    pub count: u64,
+    pub buf: Vec<WithdrawDestToken>,
+}
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct TargetOrder {
+    pub price: u64,
+    pub vol: u64,
+}
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct OutPutData {
+    pub need_take_pnl_coin: u64,
+    pub need_take_pnl_pc: u64,
+    pub total_pnl_pc: u64,
+    pub total_pnl_coin: u64,
+    pub pool_open_time: u64,
+    pub punish_pc_amount: u64,
+    pub punish_coin_amount: u64,
+    pub orderbook_to_init_time: u64,
+    pub swap_coin_in_amount: u128,
+    pub swap_pc_out_amount: u128,
+    pub swap_take_pc_fee: u64,
+    pub swap_pc_in_amount: u128,
+    pub swap_coin_out_amount: u128,
+    pub swap_take_coin_fee: u64,
+}
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct AmmConfig {
+    pub pnl_owner: Pubkey,
+    pub cancel_owner: Pubkey,
+    pub pending1: [u64; 28],
+    pub pending2: [u64; 32],
+}
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct LastOrderDistance {
+    pub last_order_numerator: u64,
+    pub last_order_denominator: u64,
+}
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct NeedTake {
+    pub need_take_pc: u64,
+    pub need_take_coin: u64,
+}
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct SwapInstructionBaseIn {
+    pub amount_in: u64,
+    pub minimum_amount_out: u64,
+}
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct SwapInstructionBaseOut {
+    pub max_amount_in: u64,
+    pub amount_out: u64,
+}
