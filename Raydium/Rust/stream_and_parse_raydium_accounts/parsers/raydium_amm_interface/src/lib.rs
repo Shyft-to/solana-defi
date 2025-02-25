d275943434bfc858215574bc87303bbfddc6b015
Abhi <abhirup@shyft.to>
2025-02-25 16:37:13 +0530

    Add Raydium Account streaming

---
+++ Raydium/Rust/stream_and_parse_raydium_accounts/parsers/raydium_amm_interface/src/lib.rs
@@ -0,0 +1,9 @@
+solana_program::declare_id!("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8");
+pub mod accounts;
+pub use accounts::*;
+pub mod typedefs;
+pub use typedefs::*;
+pub mod instructions;
+pub use instructions::*;
+pub mod errors;
+pub use errors::*;
