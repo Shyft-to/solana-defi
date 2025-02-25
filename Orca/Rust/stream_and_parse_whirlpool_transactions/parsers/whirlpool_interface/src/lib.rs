622c83bcb1cd46b72f336f8d185e232688b28004
AbhirupAbhi <abhirup@shyft.to>
2025-02-25 17:55:13 +0530

    Added Whirlpool parsed Txns

---
+++ Orca/Rust/stream_and_parse_whirlpool_transactions/parsers/whirlpool_interface/src/lib.rs
@@ -0,0 +1,10 @@
+solana_program::declare_id!("whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc");
+pub mod accounts;
+pub use accounts::*;
+pub mod typedefs;
+pub use typedefs::*;
+pub mod instructions;
+pub use instructions::*;
+pub mod errors;
+pub use errors::*;
+pub mod serializer;
