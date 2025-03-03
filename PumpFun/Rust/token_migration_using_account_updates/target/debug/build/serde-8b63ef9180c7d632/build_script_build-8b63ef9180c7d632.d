4515533376c7f89e6c232d3f8a44405aef9fa6a2
Abhi <abhirup@shyft.to>
2025-03-03 18:22:17 +0530

    Add Meteora accounts ts

--- PumpFun/Rust/token_migration_using_account_updates/target/debug/build/serde-8b63ef9180c7d632/build_script_build-8b63ef9180c7d632.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Asus\Projects\Toolings\Shyft\PumpFun\Rust\token_migration_using_account_updates\target\debug\build\serde-8b63ef9180c7d632\build_script_build-8b63ef9180c7d632.exe: C:\Users\Asus\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.217\build.rs
-
-C:\Users\Asus\Projects\Toolings\Shyft\PumpFun\Rust\token_migration_using_account_updates\target\debug\build\serde-8b63ef9180c7d632\build_script_build-8b63ef9180c7d632.d: C:\Users\Asus\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.217\build.rs
-
-C:\Users\Asus\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.217\build.rs:
