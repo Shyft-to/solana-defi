190196904ee31ebda924f0d239a9ba29814cf694
Abhirup <97870687+AbhirupAbhi@users.noreply.github.com>
2025-03-03 18:31:42 +0530

    Add Meteora accounts ts (#25)
    Co-authored-by: Abhi <abhirup@shyft.to>

--- PumpFun/Rust/token_migration_using_account_updates/target/debug/deps/cfg_if-c17719caf675be0f.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Asus\Projects\Toolings\Shyft\PumpFun\Rust\token_migration_using_account_updates\target\debug\deps\libcfg_if-c17719caf675be0f.rmeta: C:\Users\Asus\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\Asus\Projects\Toolings\Shyft\PumpFun\Rust\token_migration_using_account_updates\target\debug\deps\libcfg_if-c17719caf675be0f.rlib: C:\Users\Asus\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\Asus\Projects\Toolings\Shyft\PumpFun\Rust\token_migration_using_account_updates\target\debug\deps\cfg_if-c17719caf675be0f.d: C:\Users\Asus\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\Asus\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs:
