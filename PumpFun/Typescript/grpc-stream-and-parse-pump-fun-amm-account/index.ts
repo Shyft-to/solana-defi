7d7009a8c4a4bd5ac06f0dec23cd61616e9ad726
Mona <ahyammonapaul5@gmail.com>
2025-04-02 14:25:00 -0100

    Pumpfun AMM Account

---
+++ PumpFun/Typescript/grpc-stream-and-parse-pump-fun-amm-account/index.ts
@@ -0,0 +1,124 @@
+import "dotenv/config";
+import Client, {
+  CommitmentLevel,
+  SubscribeRequestAccountsDataSlice,
+  SubscribeRequestFilterAccounts,
+  SubscribeRequestFilterBlocks,
+  SubscribeRequestFilterBlocksMeta,
+  SubscribeRequestFilterEntry,
+  SubscribeRequestFilterSlots,
+  SubscribeRequestFilterTransactions,
+} from "@triton-one/yellowstone-grpc";
+import { SubscribeRequestPing } from "@triton-one/yellowstone-grpc/dist/grpc/geyser";
+import { Connection, PublicKey, VersionedTransactionResponse } from "@solana/web3.js";
+import { tOutPut } from "./utils/transactionOutput";
+import { publicKey } from "@solana/buffer-layout-utils";
+
+import { Boolean } from "@solana/buffer-layout";
+
+
+const PUMP_FUN_AMM_PROGRAM_ID = new PublicKey(
+  "pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA"
+);
+
+interface SubscribeRequest {
+  accounts: { [key: string]: SubscribeRequestFilterAccounts };
+  slots: { [key: string]: SubscribeRequestFilterSlots };
+  transactions: { [key: string]: SubscribeRequestFilterTransactions };
+  transactionsStatus: { [key: string]: SubscribeRequestFilterTransactions };
+  blocks: { [key: string]: SubscribeRequestFilterBlocks };
+  blocksMeta: { [key: string]: SubscribeRequestFilterBlocksMeta };
+  entry: { [key: string]: SubscribeRequestFilterEntry };
+  commitment?: CommitmentLevel | undefined;
+  accountsDataSlice: SubscribeRequestAccountsDataSlice[];
+  ping?: SubscribeRequestPing | undefined;
+}
+
+async function handleStream(client: Client, args: SubscribeRequest) {
+  // Subscribe for events
+  console.log("Stream Starting...")
+  const stream = await client.subscribe();
+
+  // Create `error` / `end` handler
+  const streamClosed = new Promise<void>((resolve, reject) => {
+    stream.on("error", (error) => {
+      console.log("ERROR", error);
+      reject(error);
+      stream.end();
+    });
+    stream.on("end", () => {
+      resolve();
+    });
+    stream.on("close", () => {
+      resolve();
+    });
+  });
+
+  // Handle updates
+  stream.on("data", async (data) => {
+    if(data.account) {
+      try {
+        const result = await tOutPut(data);
+        console.log(result);
+      } catch (error) {
+        if (error) {
+          console.log(error)
+        }
+      }
+    }
+    
+  });
+
+  // Send subscribe request
+  await new Promise<void>((resolve, reject) => {
+    stream.write(args, (err: any) => {
+      if (err === null || err === undefined) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  }).catch((reason) => {
+    console.error(reason);
+    throw reason;
+  });
+
+  await streamClosed;
+}
+
+async function subscribeCommand(client: Client, args: SubscribeRequest) {
+  while (true) {
+    try {
+      await handleStream(client, args);
+    } catch (error) {
+      console.error("Stream error, restarting in 1 second...", error);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+}
+const client = new Client(
+  process.env.GRPC_URL,
+  process.env.X_TOKEN,
+  undefined,
+);
+
+const req: SubscribeRequest = {
+  "slots": {},
+  "accounts": {
+    "pumpfun": {
+      "account": [],
+      "filters": [],
+      "owner": [PUMP_FUN_AMM_PROGRAM_ID.toBase58()]
+    }
+  },
+  "transactions": {},
+  "blocks": {},
+  "blocksMeta": {
+    "block": []
+  },
+  "accountsDataSlice": [],
+  "commitment": CommitmentLevel.PROCESSED, // Subscribe to processed blocks for the fastest updates
+  entry: {},
+  transactionsStatus: {}
+}
+subscribeCommand(client, req);
