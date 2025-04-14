7185a6d983b2b0b0283b4af4f5cfe3205dd96d9e
ahyammona <37624177+ahyammona@users.noreply.github.com>
2025-04-14 16:30:03 -0100

    Hotfix/raydium buy sell event (#45)
    * Raydium Buy Sell Event
    
    * Raydium AMM BUY SELL EVENTS
    
    * Update transaction-event-parser.ts
    
    * Update package.json
    
    * Update README.md
    
    * Update transaction-event-parser.ts
    
    * Update package.json

---
+++ Raydium/Typescript/stream_raydium_amm_transactions_and_detect_buy_sell_events/README.md
@@ -0,0 +1,36 @@
+# Stream Raydium AMM transactions and events, and detect buy and sell events
+
+This project streams and parses Raydium AMM transactions on the Solana blockchain in real time.
+By monitoring buy and sell events as they occur,
+it delivers valuable insights into market activity.
+This enables users to better understand market trends and user behavior within the Raydium AMM ecosystem.
+
+![screenshot](assets/raydium-event.png?raw=true "Screenshot")
+
+## Getting Started
+
+1. **Clone the repository:**
+   ```bash
+   git clone https://github.com/Shyft-to/solana-defi.git
+   cd Raydium/Typescript/stream_raydium_amm_transactions_and_detect_buy_sell_events
+   ```
+
+2. **Install Dependencies:**
+
+    ```bash
+    # For example, if using npm
+    npm i
+    ```
+
+3. **Run the script:**
+
+    ```bash
+    # To run the script
+    npm run start
+    ```
+
+*Note: Please on the `.env` file to  input your env details before running the script.*
+
+## Related Links
+
+Shyft gRPC Docs: [https://docs.shyft.to/solana-fast-grpc/grpc-docs]
---
+++ Raydium/Typescript/stream_raydium_amm_transactions_and_detect_buy_sell_events/utils/transaction-event-parser.ts
@@ -0,0 +1,72 @@
+const sol = 'So11111111111111111111111111111111111111112'
+const usdc = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
+
+export function transactionEventParser(txn,parsedTxn){
+ const pTB = txn.meta.postTokenBalances;
+ const mints = pTB.find(
+  (token) => token.mint !== sol && token.mint !== usdc
+ )?.mint;
+ let userSourceOwner = parsedTxn.instructions
+    .flatMap((i) => i.accounts)
+    .find((a) => a.name === 'userSourceOwner' && a.isSigner)?.pubkey;
+
+  if (!userSourceOwner) {
+    userSourceOwner = parsedTxn.instructions
+      .flatMap((i) => i.accounts)
+      .find((a) => a.isSigner)?.pubkey;
+  }
+
+ const eventName = parsedTxn.events[0].name;
+ const data = parsedTxn.events[0].data;
+ const poolCoinTokenAccount = parsedTxn.instructions
+     .flatMap((i)=> i.accounts)
+     .find((a) => a.name === 'poolCoinTokenAccount')?.pubkey;
+const determineMarket = (amountIn: number) => {
+    const transferInstruction = parsedTxn.instructions.find(
+      (instruction) =>
+        instruction.name === 'transfer' &&
+        instruction.args?.amount === amountIn
+    );
+
+    if (transferInstruction) {
+      const destinationAccount = transferInstruction.accounts.find(
+        (account) => account.name === 'destination'
+      )?.pubkey;
+
+      return destinationAccount === poolCoinTokenAccount && (mints !== sol);
+    }
+
+    return false;
+  };
+
+ console.log("userSourceOwner :", userSourceOwner);
+ if(eventName == "swapBaseIn"){
+   const signer = userSourceOwner;
+    const mint = mints;
+    const amount_in = data.amountIn;
+    const amount_out = data.outAmount;
+    const type = determineMarket(amount_in) ? "Sell" : "Buy";
+    return {
+       "Event Name: ": eventName,
+        "User: " : signer,
+        "Mint: " : mint,
+        "Amount in: ": amount_in,
+        "Amount out: " : amount_out,
+        "Type: " : type
+    }
+ }else if(eventName == "swapBaseOut"){
+     const signer = userSourceOwner;
+     const mint = mints;
+     const amount_in = data.directIn;
+     const amount_out = data.amountOut;
+     const type = determineMarket(amount_in) ? "Sell" : "Buy";
+     return {
+        "Event Name: ": eventName,
+        "User: ": signer,
+        "Mint: " : mint,
+        "Amount in: ": amount_in,
+        "Amount out: " : amount_out,
+        "Type: " : type
+     }
+ }
+}
