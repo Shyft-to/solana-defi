477e609970225a6d4a12feac3a69624ceaae5796
ahyammona <37624177+ahyammona@users.noreply.github.com>
2025-04-25 06:35:15 -0100

    Pumpfun Amm New Pool (#54)
    * Pumpfun Amm New Pool
    
    * Update .env.example

---
+++ PumpFun/Typescript/stream_new_pool_pumpfun_amm_transactions/utils/pumpfun-amm-txn-formatter.ts
@@ -0,0 +1,39 @@
+export function pumpfun_amm_formatter(parsedInstruction,txn){
+    let output = {};
+    const instruction_name = parsedInstruction.instructions.find((x)=> x.name === 'create_pool')
+    if(!instruction_name)return;
+    if(txn.version === 0){
+      output = {
+        ...txn,
+        meta: {
+          ...txn.meta,
+          innerInstructions: parsedInstruction.inner_ixs,
+        },
+        transaction: {
+          ...txn.transaction,
+          message: {
+            ...txn.transaction.message,
+            compiledInstructions: parsedInstruction.instructions,
+          },
+        }
+      }
+    }
+    else {
+      output = {
+        ...txn,
+        meta: {
+          ...txn.meta,
+          innerInstructions: parsedInstruction.inner_ixs,
+        },
+        transaction: {
+          ...txn.transaction,
+          message: {
+            ...txn.transaction.message,
+            instructions: parsedInstruction.instructions,
+          },
+        }
+      }
+    }
+  
+    return output;
+  }
