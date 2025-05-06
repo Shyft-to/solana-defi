45496c387003823884934ce7f227a8f936856b0d
Mona <ahyammonapaul5@gmail.com>
2025-05-06 16:28:13 -0100

    Update

--- PumpFun/Typescript/stream_pump_fun_amm_transactions_and_detect_buy_sell_events/utils/swapTransactionParser.ts
+++ PumpFun/Typescript/stream_pump_fun_amm_transactions_and_detect_buy_sell_events/utils/swapTransactionParser.ts
@@ -7,13 +7,13 @@ export function parseSwapTransactionOutput(parsedInstruction, transaction) {
     );
 
     if (!swapInstruction) {
-        console.error("Instruction with name 'buy' or 'sell' not found");
-        return transaction;
+        return;
     }
 
     const signerPubkey = swapInstruction.accounts.find((account) => account.name === 'user')?.pubkey;
+    const user_quote_token_account = swapInstruction.accounts.find((account) => account.name === "user_quote_token_account")?.pubkey;
+    const pool_base_token_account = swapInstruction.accounts.find((account) => account.name === "pool_base_token_account")?.pubkey;
 
-    // Determine the amount based on the instruction type
     const swapAmount = swapInstruction.name === 'sell'
         ? swapInstruction.args?.base_amount_in
         : swapInstruction.args?.base_amount_out;
@@ -22,6 +22,16 @@ export function parseSwapTransactionOutput(parsedInstruction, transaction) {
         ? swapInstruction.args?.min_quote_amount_out
         : swapInstruction.args?.max_quote_amount_in;
 
+    const determineOutAmount = () => {
+        if (!transaction.meta.innerInstructions) {
+            console.error("No inner instructions found in transaction");
+            return null;
+        }
+         const transferChecked = parsedInstruction.inner_ixs.find(
+         (instruction) =>
+         instruction.name === 'transferChecked' && instruction.args?.amount !== swapAmount).args?.amount;
+          return transferChecked;
+    };
     const determineBuySellEvent = () => {
         const baseMintPubkey = swapInstruction.accounts.find((account) => account.name === 'base_mint')?.pubkey;
         const quoteMintPubkey = swapInstruction.accounts.find((account) => account.name === 'quote_mint')?.pubkey;
@@ -38,14 +48,25 @@ export function parseSwapTransactionOutput(parsedInstruction, transaction) {
     };
 
     const buySellEvent = determineBuySellEvent();
+  const base_amount_in = swapInstruction.name === 'sell'
+        ? swapInstruction.args?.base_amount_in
+        : swapInstruction.args?.base_amount_out;
+     
+    const amountIn = swapInstruction.name === 'buy'
+        ? determineOutAmount()
+        : base_amount_in;
 
+ const amountOut = swapInstruction.name === 'sell'
+        ? determineOutAmount()
+        : base_amount_in;
     const transactionEvent = {
         type: buySellEvent.type,
         user: signerPubkey,
         mint: buySellEvent.mint,
-        amount: swapAmount,
-        quoteAmount: quoteAmount,
+        out_amount: amountOut,
+        in_amount: amountIn, 
     };
+
 
     output = {
         ...transaction,
