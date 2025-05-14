d8f1f95c2c9cb1ace6ba1913763400871703d264
Mona <ahyammonapaul5@gmail.com>
2025-05-14 21:03:24 -0100

    Updated Name Change

--- PumpFun/Typescript/grpc-stream-and-parse-pump-fun-amm-account/utils/base-encoding.ts
+++ PumpFun/Typescript/grpc-stream-and-parse-pump-fun-amm-account/utils/base-encoding.ts
@@ -1,6 +1,6 @@
 import base58 from "bs58";
 
-export function decodeTransact(data){
+export function convertBase64ToBase58(data){
     const output = base58.encode(Buffer.from(data,'base64'))
     return output;
 }
