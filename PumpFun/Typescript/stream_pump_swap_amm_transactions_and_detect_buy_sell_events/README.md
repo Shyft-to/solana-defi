eefa14469ddce381a86922b4f6337724dc38dbae
Mona <ahyammonapaul5@gmail.com>
2025-06-05 13:02:05 -0100

    Update File and Name change

---
+++ PumpFun/Typescript/stream_pump_swap_amm_transactions_and_detect_buy_sell_events/README.md
@@ -0,0 +1,33 @@
+# Real-Time Pump Swap AMM Transaction Tracker on Solana
+
+This project provides real-time streaming and parsing of Pump Swap AMM transactions on the Solana blockchain. By monitoring live buy and sell events, it delivers actionable insights into market activity, user behavior, and trading trends within the Pump Swap Amm ecosystem. Ideal for developers, traders, and analysts seeking to understand Solana's decentralized finance (DeFi) dynamics.
+
+![screenshot](assets/amm-buy-sell.png?raw=true "Screenshot")
+
+## Getting Started
+
+1. **Clone the repository:**
+   ```bash
+   git clone https://github.com/Shyft-to/solana-defi.git
+   cd PumpFun/Typescript/stream_pump_swap_amm_transactions_and_detect_buy_sell_events
+   ```
+
+2. **Install Dependencies:**
+
+    ```bash
+    # For example, if using npm
+    npm i
+    ```
+
+3. **Run the script:**
+
+    ```bash
+    # To run the script
+    npm run start
+    ```
+
+*Note: Please rename the `.env.sample` file to `.env` and input your env details before running the script.*
+
+## Related Links
+
+Shyft gRPC Docs: [https://docs.shyft.to/solana-fast-grpc/grpc-docs]
---
+++ PumpFun/Typescript/stream_pump_swap_amm_transactions_and_detect_buy_sell_events/utils/swapTransactionParser.ts
@@ -0,0 +1,87 @@
+export function parseSwapTransactionOutput(parsedInstruction, transaction) {
+    const SOL_MINT = 'So11111111111111111111111111111111111111112';
+    let output = {};
+
+    const swapInstruction = parsedInstruction.instructions.pumpAmmIxs.find(
+        (instruction) => instruction.name === 'buy' || instruction.name === 'sell'
+    );
+
+    if (!swapInstruction) {
+        return;
+    }
+
+    const signerPubkey = swapInstruction.accounts.find((account) => account.name === 'user')?.pubkey;
+    const user_quote_token_account = swapInstruction.accounts.find((account) => account.name === "user_quote_token_account")?.pubkey;
+    const pool_base_token_account = swapInstruction.accounts.find((account) => account.name === "pool_base_token_account")?.pubkey;
+
+    const swapAmount = swapInstruction.name === 'sell'
+        ? swapInstruction.args?.base_amount_in
+        : swapInstruction.args?.base_amount_out;
+
+    const quoteAmount = swapInstruction.name === 'sell'
+        ? swapInstruction.args?.min_quote_amount_out
+        : swapInstruction.args?.max_quote_amount_in;
+
+    const determineOutAmount = () => {
+        if (!transaction.meta.innerInstructions) {
+            console.error("No inner instructions found in transaction");
+            return null;
+        }
+         const transferChecked = parsedInstruction.inner_ixs.find(
+         (instruction) =>
+         instruction.name === 'transferChecked' && instruction.args?.amount !== swapAmount).args?.amount;
+          return transferChecked;
+    };
+    const determineBuySellEvent = () => {
+        const baseMintPubkey = swapInstruction.accounts.find((account) => account.name === 'base_mint')?.pubkey;
+        const quoteMintPubkey = swapInstruction.accounts.find((account) => account.name === 'quote_mint')?.pubkey;
+
+        if (!baseMintPubkey || !quoteMintPubkey) {
+            console.error("Base or quote mint not found in swap accounts");
+            return { type: "Unknown", mint: null };
+        }
+
+        const mint = baseMintPubkey === SOL_MINT ? quoteMintPubkey : baseMintPubkey;
+        const eventType = swapInstruction.name === 'buy' ? "Buy" : "Sell";
+
+        return { type: eventType, mint };
+    };
+
+    const buySellEvent = determineBuySellEvent();
+  const base_amount_in = swapInstruction.name === 'sell'
+        ? swapInstruction.args?.base_amount_in
+        : swapInstruction.args?.base_amount_out;
+     
+    const amountIn = swapInstruction.name === 'buy'
+        ? determineOutAmount()
+        : base_amount_in;
+
+ const amountOut = swapInstruction.name === 'sell'
+        ? determineOutAmount()
+        : base_amount_in;
+    const transactionEvent = {
+        type: buySellEvent.type,
+        user: signerPubkey,
+        mint: buySellEvent.mint,
+        out_amount: amountOut,
+        in_amount: amountIn, 
+    };
+
+
+    output = {
+        ...transaction,
+        meta: {
+            ...transaction.meta,
+            innerInstructions: parsedInstruction.inner_ixs,
+        },
+        transaction: {
+            ...transaction.transaction,
+            message: {
+                ...transaction.transaction.message,
+                compiledInstructions: parsedInstruction.instructions,
+            },
+        }
+    };
+
+    return { output, transactionEvent };
+}
