abeb50f013f55e37e667e169d6a3a77dd1e25d87
Mona <ahyammonapaul5@gmail.com>
2025-10-22 12:27:37 +0100

    RabbitStream Pump Fun New Minted Token

--- Rabbitstream/Pumpfun/stream_pump_fun_new_minted_tokens/index.ts
+++ Rabbitstream/Pumpfun/stream_pump_fun_new_minted_tokens/index.ts
@@ -1,13 +1,7 @@
 import "dotenv/config";
 import Client, {
   CommitmentLevel,
-  SubscribeRequestAccountsDataSlice,
-  SubscribeRequestFilterAccounts,
-  SubscribeRequestFilterBlocks,
-  SubscribeRequestFilterBlocksMeta,
-  SubscribeRequestFilterEntry,
-  SubscribeRequestFilterSlots,
-  SubscribeRequestFilterTransactions,
+  SubscribeRequest,
 } from "@triton-one/yellowstone-grpc";
 import { PublicKey, VersionedTransactionResponse } from "@solana/web3.js";
 import { Idl } from "@coral-xyz/anchor";
@@ -54,18 +48,6 @@ console.error = (message?: any, ...optionalParams: any[]) => {
   originalConsoleError(message, ...optionalParams); 
 };
 
-interface SubscribeRequest {
-  accounts: { [key: string]: SubscribeRequestFilterAccounts };
-  slots: { [key: string]: SubscribeRequestFilterSlots };
-  transactions: { [key: string]: SubscribeRequestFilterTransactions };
-  transactionsStatus: { [key: string]: SubscribeRequestFilterTransactions };
-  blocks: { [key: string]: SubscribeRequestFilterBlocks };
-  blocksMeta: { [key: string]: SubscribeRequestFilterBlocksMeta };
-  entry: { [key: string]: SubscribeRequestFilterEntry };
-  commitment?: CommitmentLevel | undefined;
-  accountsDataSlice: SubscribeRequestAccountsDataSlice[];
-  ping?: SubscribeRequestPing | undefined;
-}
 
 const TXN_FORMATTER = new TransactionFormatter();
 const PUMP_FUN_PROGRAM_ID  = new PublicKey(
