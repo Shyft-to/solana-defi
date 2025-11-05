19c20259fa09fa97847602ffe557238498793d58
Mona <ahyammonapaul5@gmail.com>
2025-11-05 16:26:39 +0100

    Rabbitstream Pumpfun PumpAmm

---
+++ Rabbitstream/Pumpfun/Typescript/stream_pumpfun_to_pumpAmm_migration_transactions/utils/pump-fun-parsed-transaction.ts
@@ -0,0 +1,5 @@
+﻿export function pumpFunParsedTransaction(parsedInstruction,txn){
+  const filterMintTransactions = parsedInstruction.inner_ixs.find((x)=> x.name === "migrate")
+
+  return filterMintTransactions;
+}
