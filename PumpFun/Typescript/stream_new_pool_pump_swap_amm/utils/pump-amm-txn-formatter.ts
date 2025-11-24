97bc7b253032c30545378ac96fe3df8e7c9d9a43
Mona <ahyammonapaul5@gmail.com>
2025-11-24 18:19:08 +0100

    Updating New Pool Pump AMm

--- PumpFun/Typescript/stream_new_pool_pump_swap_amm/utils/pump-amm-txn-formatter.ts
+++ PumpFun/Typescript/stream_new_pool_pump_swap_amm/utils/pump-amm-txn-formatter.ts
@@ -1,39 +1,36 @@
-export function pump_amm_formatter(parsedInstruction,txn){
-    let output = {};
-    const instruction_name = parsedInstruction.instructions.find((x)=> x.name === 'create_pool')
-    if(!instruction_name)return;
-    if(txn.version === 0){
-      output = {
-        ...txn,
-        meta: {
-          ...txn.meta,
-          innerInstructions: parsedInstruction.inner_ixs,
-        },
-        transaction: {
-          ...txn.transaction,
-          message: {
-            ...txn.transaction.message,
-            compiledInstructions: parsedInstruction.instructions,
-          },
-        }
-      }
-    }
-    else {
-      output = {
-        ...txn,
-        meta: {
-          ...txn.meta,
-          innerInstructions: parsedInstruction.inner_ixs,
-        },
-        transaction: {
-          ...txn.transaction,
-          message: {
-            ...txn.transaction.message,
-            instructions: parsedInstruction.instructions,
-          },
-        }
-      }
-    }
+export function pump_amm_formatter(parsedInstruction, txn) {
+  let output = {};
+  const createPoolIx = parsedInstruction?.instructions.find(
+    (x) => x.name === "create_pool"
+  );
+
+  if (!createPoolIx) return;
+
+  const event = parsedInstruction.events?.[0]?.data;
+  if (!event) return;
+
+  const getPubkey = (name) =>
+    createPoolIx.accounts?.find((x) => x.name === name)?.pubkey;
+
+  const extracted = {
+    lp_mint: getPubkey("lp_mint"),
+    quote_mint: getPubkey("quote_mint"),
+    base_mint: getPubkey("base_mint"),
+    creator: getPubkey("creator"),
+    pool: getPubkey("pool"),
+
+    base_mint_decimals: event.base_mint_decimals,
+    quote_mint_decimals: event.quote_mint_decimals,
+    base_amount_in: event.base_amount_in,
+    quote_amount_in: event.quote_amount_in,
+    pool_base_amount: event.pool_base_amount,
+    pool_quote_amount: event.pool_quote_amount,
+    minimum_liquidity: event.minimum_liquidity,
+    initial_liquidity: event.initial_liquidity,
+    lp_token_amount_out: event.lp_token_amount_out,
+    is_mayhem_mode: event.is_mayhem_mode
+  };
+
   
-    return output;
-  }
+  return  extracted;
+}
