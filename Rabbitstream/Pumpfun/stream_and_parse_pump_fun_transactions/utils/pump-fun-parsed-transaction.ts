1c5e429231f2952b499475657a165fdae7eb8671
ahyammona <37624177+ahyammona@users.noreply.github.com>
2025-11-26 06:05:39 +0100

    RabbitStream Pump Fun Transaction (#183)
    * RabbitStream Pump Fun Transaction
    
    * RabbitStream Pumpfun Transaction
    
    * Pumpfun Json
    
    * Updating RabbitStream Txn
    
    * Updating Readme

---
+++ Rabbitstream/Pumpfun/stream_and_parse_pump_fun_transactions/utils/pump-fun-parsed-transaction.ts
@@ -0,0 +1,20 @@
+﻿export function pumpFunParsedTransaction(parsedInstruction,txn){
+  let output = {};
+    output = {
+      ...txn,
+      meta: {
+        ...txn.meta,
+      },
+      transaction: {
+        ...txn.transaction,
+        message: {
+          ...txn.transaction.message,
+          instructions : parsedInstruction.instructions,
+          compiledInstructions: parsedInstruction.inner_ixs,
+        },
+      }
+    }
+  
+
+  return output;
+}
