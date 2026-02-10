b6affb13412f3bdb3f5c5641c27325329d2d65ab
ahyammona <37624177+ahyammona@users.noreply.github.com>
2026-02-10 07:28:04 +0100

    New code ts/meteora dbc buy sell event rs (#224)
    * Rabbitstream Meteora Buy Sell Events
    
    * Rabbitstream Meteora Buy Sell Events

---
+++ Rabbitstream/Meteora/Typescript/stream_meteora_dbc_and_detect_buy_sell_events/utils/meteora_dbc_transaction_output.ts
@@ -0,0 +1,42 @@
+export function meteoradbcTransactionOutput(parsedInstruction) {
+  const ixns = parsedInstruction?.instructions;
+  if (!Array.isArray(ixns)) return;
+
+  const swapTxn = ixns.find(
+    (ix) => ix.name === 'swap' || ix.name === 'swap2'
+  );
+  if (!swapTxn) return;
+
+  const params = swapTxn?.args?.params ?? {};
+
+  const getAccount = (name) =>
+    swapTxn.accounts.find((a) => a.name === name)?.pubkey;
+
+  const baseMint = getAccount("base_mint");
+  const quoteMint = getAccount("quote_mint");
+  const payer = getAccount("payer");
+
+  let amountIn;
+  let amountOut;
+  let type;
+
+  if (swapTxn.name === "swap2") {
+    amountIn = params.amount_0;
+    amountOut = params.amount_1;
+    type = params.swap_mode === 0 ? "Sell" : "Buy";
+  } else {
+    amountIn = params.amount_in;
+    amountOut = params.minimum_amount_out;
+
+    type = amountIn < amountOut ? "Buy" : "Sell";
+  }
+
+  return {
+    Type: type,
+    BaseMint: baseMint,
+    QuoteMint: quoteMint,
+    User: payer,
+    AmountIn: amountIn,
+    AmountOut: amountOut
+  };
+}
