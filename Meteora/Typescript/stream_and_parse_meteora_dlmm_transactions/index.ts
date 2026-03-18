70b72fe7662b78e842ad5f79cf396c9721f118b8
Mona Paul <ahyammonapaul5@gmail.com>
2026-03-18 10:51:32 +0100

    Updating Meteora Dlmm

--- Meteora/Typescript/stream_and_parse_meteora_dlmm_transactions/index.ts
+++ Meteora/Typescript/stream_and_parse_meteora_dlmm_transactions/index.ts
@@ -1,57 +1,54 @@
-require('dotenv').config()
+import "dotenv/config";
 import Client, {
   CommitmentLevel,
-  SubscribeRequestAccountsDataSlice,
-  SubscribeRequestFilterAccounts,
-  SubscribeRequestFilterBlocks,
-  SubscribeRequestFilterBlocksMeta,
-  SubscribeRequestFilterEntry,
-  SubscribeRequestFilterSlots,
-  SubscribeRequestFilterTransactions,
+  SubscribeRequest,
 } from "@triton-one/yellowstone-grpc";
-import { SubscribeRequestPing } from "@triton-one/yellowstone-grpc/dist/grpc/geyser";
-import { PublicKey, VersionedTransactionResponse } from "@solana/web3.js";
-import { Idl } from "@project-serum/anchor";
-import { SolanaParser } from "@shyft-to/solana-transaction-parser";
 import { TransactionFormatter } from "./utils/transaction-formatter";
-import meteoraDLMMIdl from "./idls/meteora_dlmm.json";
-import { SolanaEventParser } from "./utils/event-parser";
-import { bnLayoutFormatter } from "./utils/bn-layout-formatter";
-import { transactionOutput } from "./utils/transactionOutput";
+import { METEORA_DLMM_PROGRAM_ID } from "./utils/type";
+import { MeteoraDlmmDecoder } from "./utils/decode-parser";
+import { meteoraDlmmParsedTransaction } from "./utils/meteora-dlmm-transaction-parser";
 
-interface SubscribeRequest {
-  accounts: { [key: string]: SubscribeRequestFilterAccounts };
-  slots: { [key: string]: SubscribeRequestFilterSlots };
-  transactions: { [key: string]: SubscribeRequestFilterTransactions };
-  transactionsStatus: { [key: string]: SubscribeRequestFilterTransactions };
-  blocks: { [key: string]: SubscribeRequestFilterBlocks };
-  blocksMeta: { [key: string]: SubscribeRequestFilterBlocksMeta };
-  entry: { [key: string]: SubscribeRequestFilterEntry };
-  commitment?: CommitmentLevel | undefined;
-  accountsDataSlice: SubscribeRequestAccountsDataSlice[];
-  ping?: SubscribeRequestPing | undefined;
-}
+const originalConsoleWarn = console.warn;
+const originalConsoleLog = console.log;
+const originalConsoleError = console.error;
+
+console.warn = (message?: any, ...optionalParams: any[]) => {
+  if (
+    typeof message === "string" &&
+    message.includes("Parser does not matching the instruction args")
+  ) {
+    return;
+  }
+  originalConsoleWarn(message, ...optionalParams); 
+};
+
+console.log = (message?: any, ...optionalParams: any[]) => {
+  if (
+    typeof message === "string" &&
+    message.includes("Parser does not matching the instruction args")
+  ) {
+    return; 
+  }
+  originalConsoleLog(message, ...optionalParams); 
+};
+
+console.error = (message?: any, ...optionalParams: any[]) => {
+  if (
+    typeof message === "string" &&
+    message.includes("Parser does not matching the instruction args")
+  ) {
+    return; 
+  }
+  originalConsoleError(message, ...optionalParams); 
+};
 
 const TXN_FORMATTER = new TransactionFormatter();
-const METEORA_DLMM_PROGRAM_ID = new PublicKey(
-  "LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo",
-);
-const METEORA_DLMM_IX_PARSER = new SolanaParser([]);
-METEORA_DLMM_IX_PARSER.addParserFromIdl(
-  METEORA_DLMM_PROGRAM_ID.toBase58(),
-  meteoraDLMMIdl as Idl,
-);
-const METEORA_DLMM_EVENT_PARSER = new SolanaEventParser([], console);
-METEORA_DLMM_EVENT_PARSER.addParserFromIdl(
-  METEORA_DLMM_PROGRAM_ID.toBase58(),
-  meteoraDLMMIdl as Idl,
-);
+const METEORADLMM_PARSER = new MeteoraDlmmDecoder();
 
 async function handleStream(client: Client, args: SubscribeRequest) {
-  // Subscribe for events
+  console.log("Streaming Token Price on Meteora Dlmm");
   const stream = await client.subscribe();
 
-  // Create `error` / `end` handler
   const streamClosed = new Promise<void>((resolve, reject) => {
     stream.on("error", (error) => {
       console.log("ERROR", error);
@@ -66,19 +63,31 @@ async function handleStream(client: Client, args: SubscribeRequest) {
     });
   });
 
-  // Handle updates
   stream.on("data", (data) => {
     if (data?.transaction) {
-      const txn = TXN_FORMATTER.formTransactionFromJson(
-        data.transaction,
-        Date.now(),
-      );
-      const parsedInstruction = decodeMeteoraDLMM(txn);
+      
+      try {
+        const txn = TXN_FORMATTER.formTransactionFromJson(
+          data.transaction,
+          Date.now()
+        );
 
-      if (!parsedInstruction) return;
-      const tOutput = transactionOutput(parsedInstruction,txn)
-  //     console.log(tOutput)
-       console.dir(tOutput, { depth: null });
+        const decodedParsedTxn = METEORADLMM_PARSER.decodeTxn(txn);
+        const parsedTxn = meteoraDlmmParsedTransaction(decodedParsedTxn, txn);
+        if(!parsedTxn) return;
+        
+        console.log(
+        new Date(),
+         ":",
+         `New transaction https://translator.shyft.to/tx/${txn.transaction.signatures[0]} \n`,
+         JSON.stringify(parsedTxn, null, 2) + "\n"
+       );
+       console.log(
+         "--------------------------------------------------------------------------------------------------"
+       );
+      } catch (error) {
+        console.error("Error processing transaction:", error);
+      }
     }
   });
 
@@ -99,27 +108,28 @@ async function handleStream(client: Client, args: SubscribeRequest) {
   await streamClosed;
 }
 
-async function subscribeCommand(client: Client, args: SubscribeRequest) {
+async function subscribeCommand(client: Client, args: SubscribeRequest) {  
   while (true) {
     try {
       await handleStream(client, args);
     } catch (error) {
-      console.error("Stream error, restarting in 1 second...", error);
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
   }
 }
+
 
 const client = new Client(
   process.env.GRPC_URL,
   process.env.X_TOKEN,
   undefined,
 );
+
 const req: SubscribeRequest = {
   accounts: {},
   slots: {},
   transactions: {
-    Meteora_DLMM: {
+    meteoraDlmm: {
       vote: false,
       failed: false,
       signature: undefined,
@@ -136,24 +146,4 @@ const req: SubscribeRequest = {
   ping: undefined,
   commitment: CommitmentLevel.CONFIRMED,
 };
-
-subscribeCommand(client, req);
-
-function decodeMeteoraDLMM(tx: VersionedTransactionResponse) {
-  if (tx.meta?.err) return;
-
-  const paredIxs = METEORA_DLMM_IX_PARSER.parseTransactionData(
-    tx.transaction.message,
-    tx.meta.loadedAddresses,
-  );
-
-  const meteora_DLMM_Ixs = paredIxs.filter((ix) =>
-    ix.programId.equals(METEORA_DLMM_PROGRAM_ID),
-  );
-
-  if (meteora_DLMM_Ixs.length === 0) return;
-  const events = METEORA_DLMM_EVENT_PARSER.parseEvent(tx);
-  const result = { instructions: meteora_DLMM_Ixs, events };
-  bnLayoutFormatter(result);
-  return result;
-}
+subscribeCommand(client, req).catch(console.error);
